b42ae5cb5bf1eceb85670658cd9ab58d8cdbcd2d
Rajesh-Royal <rajeshroyal896@gmail.com>
2019-12-13 18:06:27 +0530

    resource updated

---
+++ precache-manifest.b726b64460ee0611c2107076ff49c11b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f9411237a7a25b31028a4c7c1c773c1c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "58df72f0e6f49dedf236",
+    "url": "/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "948c10eae91f84126dfa",
+    "url": "/static/css/main.fdeda70a.chunk.css"
+  },
+  {
+    "revision": "58df72f0e6f49dedf236",
+    "url": "/static/js/2.9d51c19a.chunk.js"
+  },
+  {
+    "revision": "948c10eae91f84126dfa",
+    "url": "/static/js/main.49c2f5fd.chunk.js"
+  },
+  {
+    "revision": "4ab6b262ba156ff19f07",
+    "url": "/static/js/runtime~main.bfbdb5e4.js"
+  },
+  {
+    "revision": "a9a6353d76a698d2254fa60f2b1c213c",
+    "url": "/static/media/code.a9a6353d.jpg"
+  },
+  {
+    "revision": "a649c39e43923e767ea351bc668410f6",
+    "url": "/static/media/hero.a649c39e.png"
+  }
+]);
